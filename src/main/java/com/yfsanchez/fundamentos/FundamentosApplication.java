d1d7db184c120b129dcae082fad68d48702bb665
yfsanchez <yfsanchez@gmail.com>
2022-10-24 00:06:54 -0300

    agregar POJO con elementos de properties
    agregar POJO con elementos de properties

--- src/main/java/com/yfsanchez/fundamentos/FundamentosApplication.java
+++ src/main/java/com/yfsanchez/fundamentos/FundamentosApplication.java
@@ -4,6 +4,7 @@ import com.yfsanchez.fundamentos.bean.MyBean;
 import com.yfsanchez.fundamentos.bean.MyBeanWithDependency;
 import com.yfsanchez.fundamentos.bean.MyBeanWithProperties;
 import com.yfsanchez.fundamentos.component.ComponentDependency;
+import com.yfsanchez.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,13 +17,16 @@ public class FundamentosApplication implements CommandLineRunner {
 	private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
 	private MyBeanWithProperties myBeanWithProperties;
+	private UserPojo userPojo;
 
 	public FundamentosApplication(@Qualifier("componentImplTwo") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency,
-								  MyBeanWithProperties myBeanWithProperties){
+								  MyBeanWithProperties myBeanWithProperties,
+								  UserPojo userPojo){
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.myBeanWithProperties = myBeanWithProperties;
+		this.userPojo = userPojo;
 	}
 
 	public static void main(String[] args) {
@@ -35,5 +39,6 @@ public class FundamentosApplication implements CommandLineRunner {
 		this.myBean.print();
 		this.myBeanWithDependency.print();
 		System.out.println(this.myBeanWithProperties.nameComplete());
+		System.out.println(this.userPojo.toString());
 	}
 }
--- src/main/java/com/yfsanchez/fundamentos/configuration/MyConfigurationBeanWithProperties.java
+++ src/main/java/com/yfsanchez/fundamentos/configuration/MyConfigurationBeanWithProperties.java
@@ -1,11 +1,14 @@
 package com.yfsanchez.fundamentos.configuration;
 
 import com.yfsanchez.fundamentos.bean.*;
+import com.yfsanchez.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(UserPojo.class)
 public class MyConfigurationBeanWithProperties {
     @Value("${value.name}")
     private String name;
