389a79e176327c344625802aaaf9776f03129d1a
yfsanchez <yfsanchez@gmail.com>
2022-10-24 23:05:51 -0300

    Configuracion de datasource con properties y clases
    Configuracion de datasource con properties y clases

---
+++ src/main/java/com/yfsanchez/fundamentos/configuration/DataSourceConfiguration.java
@@ -0,0 +1,24 @@
+package com.yfsanchez.fundamentos.configuration;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+    @Bean
+    public DataSource createDataSourceConfiguration(){
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+
+        return dataSourceBuilder.build();
+    }
+
+
+}
