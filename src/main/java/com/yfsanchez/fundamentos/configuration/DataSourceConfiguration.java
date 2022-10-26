b02ed2cbaf197c902692afdca4dde203c5411bc4
yfsanchez <yfsanchez@gmail.com>
2022-10-25 23:38:09 -0300

    cargando propiedades desde files properties
    cargando propiedades desde files properties

--- src/main/java/com/yfsanchez/fundamentos/configuration/DataSourceConfiguration.java
+++ src/main/java/com/yfsanchez/fundamentos/configuration/DataSourceConfiguration.java
@@ -1,21 +1,32 @@
 package com.yfsanchez.fundamentos.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:connection.properties")
 public class DataSourceConfiguration {
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+    @Value("${driver}")
+    private String driver;
+    @Value("${username}")
+    private String username;
+    @Value("${password}")
+    private String password;
     @Bean
     public DataSource createDataSourceConfiguration(){
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
 
-        dataSourceBuilder.driverClassName("org.h2.Driver");
-        dataSourceBuilder.url("jdbc:h2:mem:testdb");
-        dataSourceBuilder.username("SA");
-        dataSourceBuilder.password("");
+        dataSourceBuilder.driverClassName(driver);
+        dataSourceBuilder.url(jdbcUrl);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
 
         return dataSourceBuilder.build();
     }
